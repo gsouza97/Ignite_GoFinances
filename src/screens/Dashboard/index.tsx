4a2df20d5abf0abcf357e5b242741b83aea6477a
Gabriel Souza <gabrielss_@outlook.com>
2021-11-11 23:45:48 -0300

    header e cartoes de destaque

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -2,12 +2,61 @@ import React from "react";
 
 import { Text } from "react-native";
 
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGreetings,
+  UserName,
+  UserWrapper,
+  Icon,
+  HighlightCards,
+} from "./styles";
+import { RFValue } from "react-native-responsive-fontsize";
+import HighlightCard from "../../components/HighlightCard";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/76229106?v=4",
+              }}
+            />
+            <User>
+              <UserGreetings>Olá, </UserGreetings>
+              <UserName>Gabriel</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+
+      <HighlightCards>
+        <HighlightCard
+          type="up"
+          title="Entradas"
+          amount="17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HighlightCard
+          type="down"
+          title="Saídas"
+          amount="1.259,00"
+          lastTransaction="Última saída dia 03 de abril"
+        />
+        <HighlightCard
+          type="total"
+          title="Total"
+          amount="16.141,00"
+          lastTransaction="01 à 16 de abril"
+        />
+      </HighlightCards>
     </Container>
   );
 }
